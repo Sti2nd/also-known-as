bb2f3d8a619f1c5a79518700e25ec4988538428c
Stian Jørgensrud <stianahj@gmail.com>
2021-01-12 21:24:09 +0100

    Center card

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Card from './Card/Card';
 
 export default function App() {
   return (
-    <>
-      <h1>Play Alias online</h1>
-      <p>Currently only Norwegian is supported. Click on the button to begin!</p>
+    <div className={styles.appContainer}>
+      <div className={styles.headerSection}>
+        <h1>Play Alias online</h1>
+        <p>Currently only Norwegian is supported. Click on the button to begin!</p>
+      </div>
       <Card words={["Nasjonalforsamlingen", "Advare", "Dialog", "Pløye", "Jens Stoltenberg", "Skuffelse", "Prins", "Fundere"]} />
-    </>
+    </div>
   );
 }
