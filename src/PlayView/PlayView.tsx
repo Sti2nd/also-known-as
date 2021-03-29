c90e8745e06aa4db592c61af6ceddb6af10629c1
Stian Jørgensrud <stianahj@gmail.com>
2021-03-29 19:47:55 +0200

    Add counter

--- src/PlayView/PlayView.tsx
+++ src/PlayView/PlayView.tsx
@@ -1,6 +1,7 @@
 import "swiper/swiper.min.css";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
 import PlayCard, { ICard } from "../PlayCard/PlayCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./PlayView.module.css";
@@ -9,6 +10,7 @@ import { chunkArray } from "../tools";
 export default function PlayView({ words }: ICard) {
   const NUM_WORDS_PER_CARD = 8;
   const [cardNumber, setCardNumber] = useState<number>(0);
+  const [swiperInstance, setSwiperInstance] = useState<any>();
   const cards = chunkArray(words, NUM_WORDS_PER_CARD)
     .filter((wordBatch) => wordBatch.length === NUM_WORDS_PER_CARD)
     .map((wordBatch, idx) => (
@@ -17,30 +19,49 @@ export default function PlayView({ words }: ICard) {
       </SwiperSlide>
     ));
 
+  const goToCard = (index: number) => {
+    swiperInstance.slideTo(index);
+  };
+
   const previousCard = () => {
-    if (cardNumber === 0) return;
+    if (!hasPrevious()) return;
     setCardNumber(cardNumber - 1);
+    goToCard(cardNumber - 1);
   };
 
   const nextCard = () => {
+    if (!hasNext()) return;
     setCardNumber(cardNumber + 1);
+    goToCard(cardNumber + 1);
+  };
+
+  const hasPrevious = () => {
+    return cardNumber !== 0;
+  };
+
+  const hasNext = () => {
+    return cardNumber !== cards.length;
   };
 
   return (
     <>
-      <Swiper className={styles.cardContainer} spaceBetween={50}>
+      <Swiper
+        className={styles.cardContainer}
+        spaceBetween={50}
+        onSwiper={setSwiperInstance}
+        onActiveIndexChange={(change) => setCardNumber(change.activeIndex)}
+      >
         {cards}
       </Swiper>
-      <Button
-        className="mr-4"
-        disabled={cardNumber === 0}
-        onClick={previousCard}
-      >
-        Forrige
-      </Button>
-      <Button className="ml-4" onClick={nextCard}>
-        Neste
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button disabled={!hasPrevious()} onClick={previousCard}>
+          Forrige
+        </Button>
+        <Badge variant="secondary"><h5>{cardNumber}</h5></Badge>
+        <Button disabled={!hasNext()} onClick={nextCard}>
+          Neste
+        </Button>
+      </div>
     </>
   );
 }
