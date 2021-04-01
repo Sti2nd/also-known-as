60f3583ae381c97b369eaecb9f70a2352a49b6f8
Stian Jørgensrud <stianahj@gmail.com>
2021-04-01 22:51:27 +0200

    capitalise first letter

--- src/PlayCard/PlayCard.tsx
+++ src/PlayCard/PlayCard.tsx
@@ -2,18 +2,22 @@ import styles from "./PlayCard.module.css";
 
 export interface ICard {
   words: string[];
-};
+}
 
 export default function PlayCard({ words }: ICard) {
-  const items = words.map(word => (
-    <li key={word} className={styles.cardListItem}>{word}</li>
+  const capitalizeFirstLetter = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  const items = words.map((word) => (
+    <li key={word} className={styles.cardListItem}>
+      {capitalizeFirstLetter(word)}
+    </li>
   ));
-  
+
   return (
     <div className={styles.card}>
-      <ol className={styles.cardList}>
-        {items}
-      </ol>
+      <ol className={styles.cardList}>{items}</ol>
     </div>
   );
 }
